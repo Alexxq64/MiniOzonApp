b179d4211ef23b55848f16ac738db796d3cc99ae
Alexxq64 <alexxq@list.ru>
2025-06-05 09:50:26 +0300

    before migration with user

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 # core/models.py
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True, verbose_name="Название категории")
@@ -40,3 +42,13 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+
+class Role(models.TextChoices):
+    BUYER = 'buyer', _('Покупатель')
+    SELLER = 'seller', _('Продавец')
+
+class User(AbstractUser):
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.BUYER)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
